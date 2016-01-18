c7b0e1da9b220a0c2372c024cb37502c63c49207
ZEE.KIM <129@jinzhe.net>
2016-01-18 12:23:30 +0800

    fixed button upload

--- vue-upload.js
+++ vue-upload.js
@@ -45,25 +45,17 @@
 		success: function() {}, //文件上传成功时
 		error: function() {}, //文件上传失败时,
 		done: function() {}, //文件全部上传完毕时
-
-
-
 		//获取选择文件，file控件或拖放
 		getFiles: function(e) {
-			// 取消鼠标经过样式
 			e.stopPropagation();
 			e.preventDefault();
 			// this[e.type === "dragover" ? "onDragOver" : "onDragLeave"].call(e.target);
-
 			//继续添加文件
 			this.files = e.target.files || e.dataTransfer.files;
 			for (var i = 0, f; f = this.files[i]; i++) {
 				if (f.size <= this.limit) {
 					f.index = Math.random().toString().substr(2); //增加唯一索引值
-				} else {
-					this.delete(f.name);
 				}
-
 			}
 			//执行选择回调
 			this.select(this.files);
@@ -75,33 +67,23 @@
 			for (var i = 0, file; file = this.files[i]; i++) {
 				if (file != name) {
 					arrFile.push(file);
-				} else {
-					this.delete(name);
 				}
 			}
 			this.files = arrFile;
 			return this;
 		},
-
 		//文件上传
 		go: function() {
 			var that = this;
-			if (location.host.indexOf("sitepointstatic") >= 0) {
-				//非站点服务器上运行
-				return;
-			}
-
 			for (var i = 0, f; f = this.files[i]; i++) {
 				(function(file) {
 					var xhr = new XMLHttpRequest();
 					if (xhr.upload) {
-
 						var data = new FormData();
 						data.append(that.filename, file);
 						for (key in that.data) {
 							data.append(key, that.data[key]);
 						}
-
 						// 上传中
 						xhr.upload.addEventListener("progress", function(e) {
 							that.progress(file, e.loaded, e.total);
@@ -111,16 +93,14 @@
 						xhr.onreadystatechange = function(e) {
 							if (xhr.readyState == 4) {
 								if (xhr.status == 200) {
-	 
 									if(xhr.responseText.substr(0,1)=="{"){
 										xhr.responseText=eval("("+xhr.responseText+")");
 									}
 									that.success(file, xhr.responseText);
-									that.delete(file);
 									if (!that.files.length) {
-										//全部完毕
-										that.done();
+										that.done();//全部完毕
 									}
+									if(that.auto)that.input.value="";
 								} else {
 									that.error(file, xhr.responseText);
 								}
@@ -129,10 +109,6 @@
 
 						// 开始上传
 						xhr.open("POST", that.url, true);
-						// xhr.setRequestHeader('Cache-Control', 'no-cache')
-						// xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
-						// xhr.setRequestHeader("Content-Type", "multipart/form-data");
-						// xhr.setRequestHeader('X-Request-With','XMLHttpRequest');
 						for (key in that.header) {
 							xhr.setRequestHeader(key, that.header[key]);
 						}
@@ -146,7 +122,6 @@
 
 		init: function() {
 			var that = this;
-
 			if (that.drag) {
 				that.drag.addEventListener("dragover", function(e) {
 					that.dragover(e);
@@ -170,24 +145,24 @@
 				if (that.multiple) {
 					that.input.setAttribute("multiple", "multiple");
 				}
-				that.input.addEventListener("change", function(e) {
+				var changeUpload=function(e) {
 					that.getFiles(e);
-					if (that.auto) {
-						that.go(e);
-					}
-				}, false);
+					if (that.auto) that.go();
+				}
+				that.input.removeEventListener("change",changeUpload, false);
+				that.input.addEventListener("change",changeUpload, false);
 			}
-
 			//上传按钮提交
 			if (that.button) {
-				that.button.addEventListener("click", function(e) {
-					that.go(e);
-				}, false);
+				var buttonUpload=function(e) {
+					that.go();
+				};
+				that.button.removeEventListener("click",buttonUpload, false);
+				that.button.addEventListener("click",buttonUpload, false);
 			}
 		}
 
 	};
-	
 	var VueUpload={
 		install:function(Vue){
 			Vue.prototype.$upload=function(options){
@@ -197,8 +172,7 @@
 				upload.init();
 			};
 		}
-	}
- 
+	};
 	if (typeof exports == "object") {
 		module.exports = VueUpload;
 	} else if (typeof define == "function" && define.amd) {
@@ -208,5 +182,5 @@
 	} else if (window.Vue) {
 		window.VueUpload = VueUpload;
 		Vue.use(VueUpload);
-	};
+	}
 })();
