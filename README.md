9f761dfaa6792bbde0f97735a895fca1acd034e2
ZEE.KIM <129@jinzhe.net>
2016-09-09 17:28:09 +0800

    vue-upload

--- README.md
+++ README.md
@@ -1,42 +1,97 @@
 # vue-upload
 
-基于vuejs的上传插件,因为使用了Formdata仅仅支持ie10+。QQ讨论群: 240319632。
- 
- 
+基于vuejs+webpack环境使用的上传组件
 
-# 引入插件
-```
-var VueUpload = require('./plugins/vue-upload.js');
-Vue.use(VueUpload);
+[Live demo](http://jinzhe.github.io/vue-upload/)
+
+* 支持服务器的域名和接口单独设置
+* 支持自定义提交字段
+* 支持自定义xhr 提交的header字段/表单name/限定上传格式/文件大小／
+* 支持图片上传前预先裁剪，其中包括裁减按钮替换、宽高、质量（quality）
+* 支持自定义回调方法
+* 支持裁减图片缩放，包括移动端手势支持
+
+
+![](http://ww1.sinaimg.cn/large/823603acgw1ez1n57t8jug20d40acwjw.gif)
+
+
+# Options
+
+* :server   (string)  服务器地址 // Server host,like "http://jinzhe.net"
+* :api   (string)      // Server api path,like "/api/v1/getdata/"
+* :params  (object) 	额外附加的字段，它是一个object
+* :filename   (string)  文件表单名称，默认为file
+* :file   (string)    返回的文件名或地址
+* :ext    (string)    限制文件格式如“jpg,png,zip”
+* :header   (object)   提交服务端的头部字段，它是一个object
+* :limit  (int)  限制大小
+* :multiple	 (bool) 多文件上传
+* :preview   (bool)	预览图片
+* :auto	     (bool)	是否自动上传
+* :crop      (bool) 是否开启裁减
+* :width      (int) 裁减宽度
+* :height      (int) 裁减高度
+* :quality      (float) 裁减质量 （0～1），默认0.8
+* :ok   (string)    确定裁减显示文本
+* :cancel   (string)    取消裁减显示文本
+* :container   (DOM)    包含组件的根dom节点，这个是为了在网页内容很长的情况下为了显示正常使用的（移动滚动条），不设置默认指向document.body。
+* :success    (function)    成功上传回调 
+
+# How to use?
 
 ```
-# 组件上应用
+<template>
+<div class="layout">
+    <div class="demo">
+        <upload 
+        :server="upload.server" 
+        :api="upload.api" 
+        :params="upload.params"
+        :success="upload.success"
+        :file.sync="upload.file"
+        :crop="upload.crop"
+        :width="upload.width"
+        :height="upload.height"
+        :ok="upload.ok" 
+        :cancel="upload.cancel">
+            <img src="./upload.svg">
+        </upload>
+    </div>
+</div>
+</template>
 
-```
 <script>
-module.exports = {
-    ready:function(){
-        // 上传绑定
-        this.$upload({
-            input:document.querySelector("#upload"),
-            url:"/api/upload/",
-            // 额外附加字段
-            data:{
-                token:'xxx'
+import upload from "./upload.vue"
+export default {
+    components:{
+        upload
+    },
+    data() {
+        return {
+            upload:{
+                server:"",
+                api:"",
+                params:{
+                    token:"test"
+                },
+                file:"",
+                preview:true,
+                crop:true,
+                width:400,
+                height:400,
+                cancel:"取消",
+                ok:"裁剪",
+                success:(data)=>{
+                    alert(data.length)
+                }
             },
-            // 无法连接网络
-            error:function(file,responseText){
-                
-            },
-            // 进度
-            progress:function(file,loaded,total){
-
-            },
-            success:function(file,data){
-                data=JSON.parse(data);
-            }
-        });
+        }
     }
 }
 </script>
 ```
+ 
+# Conatct
+- Author: [zee](http://jinzhe.net)
+- Tencent QQ Group: 240319632
+- E-mail: 129@jinzhe.net
---
+++ src/app.vue
@@ -0,0 +1,270 @@
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    border: 0;
+    font-family: "Helvetica Neue","Arial","PingFang SC","Hiragino Sans GB","STHeiti","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;    -webkit-tap-highlight-color:rgba(0, 0, 0, 0);
+    -webkit-touch-callout:none;
+    -webkit-font-smoothing: antialiased;
+}
+b{
+    font-weight: 600;
+}
+svg:not(:root) {
+    overflow: hidden;
+}
+img {
+    vertical-align: middle
+}
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+}
+a {
+    text-decoration: none;
+    color: #353535;
+}
+a:hover {
+    text-decoration: none;
+}
+abbr[title] {
+    border-bottom: 1px dotted;
+}
+b, strong {
+    font-weight: 600;
+}
+dfn {
+    font-style: italic;
+}
+mark {
+    background: #ff0;
+    color: #000;
+}
+code, kbd, pre, samp {
+    font-family: monospace, serif;
+    font-size: 1em;
+}
+pre {
+    white-space: pre-wrap;
+}
+small {
+    font-size: 80%;
+}
+sub, sup {
+    font-size: 75%;
+    line-height: 0;
+    position: relative;
+    vertical-align: baseline;
+}
+sup {
+    top: -0.5em;
+}
+sub {
+    bottom: -0.25em;
+}
+
+h1, h2, h3, h4, h5, h6 {
+    font-family: inherit;
+    font-weight: bold;
+    margin: 8px 0;
+    line-height: 120%;
+}
+article, aside, details, figcaption, figure, footer, header, hgroup, main, nav, section, summary {
+    display: block;
+}
+audio, canvas, video {
+    display: inline-block;
+}
+audio:not([controls]) {
+    display: none;
+    height: 0;
+}
+address, caption, cite, code, dfn, em, th, var {
+    font-style: normal;
+    font-weight: 500;
+}
+/* placeholder focus */
+input:focus::-webkit-input-placeholder ,
+textarea:focus::-webkit-input-placeholder {
+    color: transparent;
+}
+input:focus::-moz-input-placeholder,
+textarea:focus::-moz-input-placeholder {
+    color: transparent;
+}
+input:focus::-ms-input-placeholder,
+textarea:focus::-ms-input-placeholder {
+    color: transparent;
+}
+::-ms-clear {
+    display: none;
+}
+::selection {
+    background: #333;
+    color: white;
+}
+textarea {
+    overflow: auto;
+    vertical-align: top;
+    resize:none;
+}
+/* 定位对齐 */
+.pa,.absolute {
+    position: absolute
+}
+.pr,.relative {
+    position: relative;
+}
+.pf,.fixed {
+    position: fixed
+}
+/* 手指 */
+.cp,.pointer {
+    cursor: pointer;
+}
+/* 文本对齐 */
+.text.center {
+    text-align: center;
+}
+.text.left {
+    text-align: left;
+}
+.text.right {
+    text-align: right;
+}
+/* 浮动对齐 */
+.float.left, .float.right {
+    display: inline;
+}
+.float.left {
+    float: left;
+}
+.float.right {
+    float: right;
+}
+.clear {
+    clear: both;
+    zoom: 1;
+    font-size: 0;
+    height: 0;
+}
+.clear:after {
+    visibility: hidden;
+    display: block;
+    font-size: 0;
+    content: " ";
+    clear: both;
+    height: 0;
+}
+:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0px 1000px rgba(255,255,255,1) inset !important;
+}
+html,body {
+    background-color: #fff;
+    font-size: 14px;
+    font-family: "Helvetica Neue","Arial","PingFang SC","Hiragino Sans GB","STHeiti","Microsoft YaHei","WenQuanYi Micro Hei",sans-serif;
+    line-height: 100%;
+    height:100%;
+    color: #555;
+}
+
+#app{
+    height:100%;
+}
+.layout{
+    box-sizing: border-box;
+    padding-top:120px; 
+}
+.demo{
+    margin:auto;
+    padding:10px;
+    width:200px;
+    height:200px;
+    /*background:#fbfbfb;*/
+    border:1px solid #ddd;
+    line-height: 200px;
+    text-align: center;
+    border-radius:2px;
+    color:#999;
+}
+ 
+.github {
+    position: absolute;
+    top:-40px;
+    left:-90px;
+    display: block;
+    width:200px;
+    height:100px;
+    font-size:10px;
+    line-height: 160px;
+    text-align: center;
+    text-decoration: none;
+    border-bottom:1px dashed #ccc;
+    color:#555;
+    background: #fbfbfb;
+    -webkit-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+}
+.github:hover{
+    background:#efefef;
+    color:#000;
+}
+ 
+</style>
+<template>
+<div class="layout">
+    <div class="demo">
+        <upload 
+        :server="upload.server" 
+        :api="upload.api" 
+        :params="upload.params" 
+        :success="upload.success"
+        :file.sync="upload.file"
+        :crop="upload.crop"
+        :width="upload.width"
+        :height="upload.height"
+        :ok="upload.ok" 
+        :cancel="upload.cancel">
+            <img src="./upload.svg">
+        </upload>
+    </div>
+    <a class="github" href="https://github.com/jinzhe/vue-upload" target="_blank">GITHUB</a>
+</div>
+
+ 
+
+ 
+ 
+</template>
+
+<script>
+import upload from "./upload.vue"
+
+export default {
+    components:{
+        upload
+    },
+    data() {
+        return {
+            upload:{
+                server:"",
+                api:"",
+                params:{
+                    token:"test"
+                },
+                file:"",
+                preview:true,
+                crop:true,
+                width:400,
+                height:400,
+                cancel:"取消",
+                ok:"裁剪",
+                success:(data)=>{
+                    alert(data.length)
+                }
+            },
+        }
+    }
+}
+</script>
